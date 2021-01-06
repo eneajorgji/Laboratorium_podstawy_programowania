f0f0b5d39a818878bc5ae6c0937ecfd50ba37535
Enea Jorgji <jorgji.enea@gmail.cm>
2021-01-06 14:13:46 +0100

    creating ex 7 and testing it

--- test_lab_4.py
+++ test_lab_4.py
@@ -1,4 +1,10 @@
 import numpy as np
 
+# a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
 
 
+b = np.resize(a, (a.size/2, a.size/2))
+
+print(a)
+print("#" * 15)
+print(b)
