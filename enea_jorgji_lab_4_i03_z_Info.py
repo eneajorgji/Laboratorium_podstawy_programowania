b7dd50414997809f41b649f8d154085a2de05b56
Enea Jorgji <jorgji.enea@gmail.cm>
2021-01-08 22:48:50 +0100

    finalized zad 10

--- enea_jorgji_lab_4_i03_z_Info.py
+++ enea_jorgji_lab_4_i03_z_Info.py
@@ -139,10 +139,31 @@ def second_composition(n, m):
 
 n = 10
 m = 10
-
 result_second_composition = second_composition(n, m)
-
 print(result_second_composition)
 
 # Zadanie 10
 print("Zadanie 10")
+
+
+def third_composition(n, m):
+    half_n = int(n / 2)
+    half_m = int(m / 2)
+
+    up_left_one_fourth = np.full((half_n, half_m), 1)
+    up_right_one_fourth = np.full((half_n, half_m), -2)
+
+    down_left_one_fourth = np.full((half_n, half_m), 2)
+    down_right_one_fourth = np.full((half_n, half_m), -1)
+
+    first_line_up_concatenate = np.concatenate((up_left_one_fourth, up_right_one_fourth), axis=1)
+    second_line_down_concatenate = np.concatenate((down_left_one_fourth, down_right_one_fourth), axis=1)
+
+    full_matrix = np.concatenate((first_line_up_concatenate, second_line_down_concatenate), axis=0)
+
+    return full_matrix
+
+
+n = 100
+m = 100
+print(third_composition(n, m))
--- test_lab_4.py
+++ test_lab_4.py
@@ -1,17 +1,32 @@
 import numpy as np
 
 
-def second_composition(n, m):
-    upper_triangle = np.triu_indices(n, 1)
-    ones = np.cumsum(np.ones((n, m)), axis=0)
-    ones[upper_triangle] = -1
+# def third_composition(n, m):
+#     third_matrix = np.fromfunction(lambda i, j: i + j, (n, m), dtype=int)
+#     return third_matrix
+#
+#
+# print(third_composition(9, 9))
 
-    return ones
+
+def third_composition(n, m):
+    half_n = int(n / 2)
+    half_m = int(m / 2)
+
+    up_left_one_fourth = np.full((half_n, half_m), 1)
+    up_right_one_fourth = np.full((half_n, half_m), -2)
+
+    down_left_one_fourth = np.full((half_n, half_m), 2)
+    down_right_one_fourth = np.full((half_n, half_m), -1)
+
+    first_up_concatenate = np.concatenate((up_left_one_fourth, up_right_one_fourth), axis=1)
+    first_down_concatenate = np.concatenate((down_left_one_fourth, down_right_one_fourth), axis=1)
+
+    full_matrix = np.concatenate((first_up_concatenate, first_down_concatenate), axis=0)
+
+    return full_matrix
 
 
-n = 6
-m = 6
-
-result_second_composition = second_composition(n, m)
-
-print(result_second_composition)
+n = 100
+m = 100
+print(third_composition(n, m))
