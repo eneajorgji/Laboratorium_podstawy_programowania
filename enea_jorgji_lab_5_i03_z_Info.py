ec55cd28bab6bd50fab6ae99f6661e651a09c0da
Enea Jorgji <jorgji.enea@gmail.cm>
2021-01-10 16:33:09 +0100

    first imports

---
+++ enea_jorgji_lab_5_i03_z_Info.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# Zadania 1
+def simple_plot(a, b):
+    pass
+
+# Zadania 2
+# Zadania 3
+# Zadania 4
+# Zadania 5
+# Zadania 6
+# Zadania 7
+# Zadania 8
+# Zadania 9
+# Zadania 10
