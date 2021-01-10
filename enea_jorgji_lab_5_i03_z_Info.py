30fa7c257d663a357a741fe85fecce653e2badff
Enea Jorgji <jorgji.enea@gmail.cm>
2021-01-10 17:40:31 +0100

    zad_1

--- enea_jorgji_lab_5_i03_z_Info.py
+++ enea_jorgji_lab_5_i03_z_Info.py
@@ -5,7 +5,22 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Zadania 1
 def simple_plot(a, b):
-    pass
+    plt.plot((a), label="red", color="red")
+    plt.plot((b), label="blue", color="blue")
+
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+    plt.legend(bbox_to_anchor=(1.01, 1),title="Legenda", loc='upper left', ncol=1)
+
+    plt.show()
+
+
+a = [1, 4, 7, 10]
+b = [2, 5, 8, 11]
+
+res_simple_plot = simple_plot(a, b)
+print(res_simple_plot)
 
 # Zadania 2
 # Zadania 3
