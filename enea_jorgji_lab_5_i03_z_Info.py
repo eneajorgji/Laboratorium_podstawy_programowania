6ca6bf672fdfe19c7c804af5e9941654884f564b
Enea Jorgji <jorgji.enea@gmail.cm>
2021-01-24 22:26:04 +0100

    completed zadania 10

--- enea_jorgji_lab_5_i03_z_Info.py
+++ enea_jorgji_lab_5_i03_z_Info.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 
 # Zadania 1
@@ -66,8 +65,8 @@ print(result_zad_3)
 # Zadania 4
 def bar_plot(x, y):
     plt.bar(x, width=1, height=y)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.xticks([1, 2, 3, 4, 5, 6])
 
     plt.show()
@@ -101,11 +100,12 @@ print(result_heat_plot)
 # Zadania 6
 def contour_plot(x, y):
     X, Y = np.meshgrid(x, y)
+
     Z = np.sqrt((X ** 2 + Y ** 2))
     CS = plt.contour(X, Y, Z)
 
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.clabel(CS, inline=1, fontsize=10)
 
     plt.show()
@@ -157,4 +157,23 @@ m1 = np.random.randn(13, 3)
 m2 = np.random.randn(18, 4)
 result_zad_9 = make_subplots(m1, m2)
 
+
 # Zadania 10
+def make_3D(x, y):
+    fig = plt.figure(figsize=(50, 50))
+    ax = fig.gca(projection='3d')
+
+    X, Y = np.meshgrid(x, y)
+    Z = np.sqrt((X ** 2 + Y ** 2))
+
+    ax.plot_surface(X, Y, Z, cmap='jet')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+
+    plt.show()
+
+
+x = np.arange(-52, 100, 2.4)
+y = np.arange(-28, 81, 0.9)
+result_zad_10 = make_3D(x, y)
--- test_lab_5.py
+++ test_lab_5.py
@@ -2,4 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 # Zadania 10
+def make_3D(x, y):
+    fig = plt.figure(figsize=(50, 50))
+    ax = fig.gca(projection='3d')
+
+    X, Y = np.meshgrid(x, y)
+    Z = np.sqrt((X ** 2 + Y ** 2))
+
+    ax.plot_surface(X, Y, Z, cmap='jet')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+
+    plt.show()
+
+
+x = np.arange(-52, 100, 2.4)
+y = np.arange(-28, 81, 0.9)
+result_zad_10 = make_3D(x, y)
