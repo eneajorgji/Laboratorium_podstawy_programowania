6079ccd8a1031b6912cefc8d42879d6dacd6970a
Enea Jorgji <jorgji.enea@gmail.cm>
2021-01-31 17:22:12 +0100

    zad 2 and 3

--- enea_jorgji_lab_6_i03_z_Info.py
+++ enea_jorgji_lab_6_i03_z_Info.py
@@ -1,3 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Zadanie 1
+arr_zad_1 = np.random.randint(2, size=(10, 5))
+
+print(arr_zad_1)
+
+# Zadanie 2
+
+df = pd.DataFrame(arr_zad_1, columns=["col1", "col2", "col3", "col4", "col5"])
+print(df)
+
+# Zadanie 3
+for i, column in enumerate(df):
+
+
+# Zadanie 4
+# Zadanie 5
+# Zadanie 6
