19531a68f824d0d05b273d2b8cd41c9687ba7609
Enea Jorgji <jorgji.enea@gmail.cm>
2021-02-08 22:31:50 +0100

    added explenation to zad 3

--- enea_jorgji_lab_6_i03_z_Info.py
+++ enea_jorgji_lab_6_i03_z_Info.py
@@ -13,13 +13,21 @@ df = pd.DataFrame(arr_zad_1, columns=["col1", "col2", "col3", "col4", "col5"])
 print("Zadanie 2 \n", df)
 
 # Zadanie 3
+# Zliczyc ile wartosci w zadanej kolumnie jest wiekszych od 0.5, a ile mniejszych od 0.5.
 bigger_than_o5 = 0
+smaller_than_o5 = 0
+
+subset_df = df[df["col2"] >= .5]
+column_count = subset_df.count()
+
+print(column_count)
 
 # for key, value in df.iteritems():
-#     df["col1"] = df["col1"].apply(if value > 0.5)
 
-    # print("Zadanie 3 \n")
+# print(key, bigger_than_o5)
 
-    # Zadanie 4
-    # Zadanie 5
-    # Zadanie 6
+# print("Zadanie 3 \n")
+
+# Zadanie 4
+# Zadanie 5
+# Zadanie 6
