c0326390440ef1065e301e94604a771e8492137e
Enea Jorgji <jorgji.enea@gmail.cm>
2021-02-10 22:48:58 +0100

    zad 6

--- enea_jorgji_lab_6_i03_z_Info.py
+++ enea_jorgji_lab_6_i03_z_Info.py
@@ -15,30 +15,38 @@ df = pd.DataFrame(arr_zad_1, columns=["col1", "col2", "col3", "col4", "col5"])
 print("Zadanie 2 \n", df)
 
 # Zadanie 3
-# Zliczyc ile wartosci w zadanej kolumnie jest wiekszych od 0.5, a ile mniejszych od 0.5.
-bigger_than_o5 = 0
-smaller_than_o5 = 0
+df_to_sum = df.where(df < .5, True)
+df_to_sum = df_to_sum.where(df_to_sum > .5, False)
+print("Zadanie 3 \n", df_to_sum)
 
-subset_df = df[df["col2"] >= .5]
-column_count = subset_df.count()
-
-print(column_count)
-
-# for key, value in df.iteritems():
-
-# print(key, bigger_than_o5)
-
-# print("Zadanie 3 \n")
+sum_of_bigger = df_to_sum.sum()
+print("Zadanie 3 \n", sum_of_bigger)
 
 # Zadanie 4
+
+print("Zadanie 4 \n")
+
 # Zadanie 5
 df_2 = df.append(df, ignore_index=True)
-print(df_2)
+print("Zadanie 5 \n", df_2)
 
 # Zadanie 6
-arr_zad_6 = np.random.randint(2, size=(20, 5))
+arr_zad_6 = np.random.uniform(0, 1, size=(20, 5))
 df_zad_6 = pd.DataFrame(arr_zad_6, columns=["col1", "col2", "col3", "col4", "col5"])
-print(df_zad_6)
+# print("Zadanie 6 \n", df_zad_6)
 
-df_zad_6_joined = pd.concat([df_zad_6, df_2], axis=1)
-print(df_zad_6_joined)
+df_concat = df_2.copy()
+df_concat.columns = ["col6", "col7", "col8", "col9", "col10"]
+df_zad_6 = pd.concat([df_zad_6, df_concat], axis=1)
+print("Zadanie 6 \n", df_zad_6)
+
+df_zad_6_sort = df_zad_6.sort_values(by=["col1", "col6"])
+print("Zadanie 6 - sort by col1 and col 6 \n", df_zad_6_sort)
+
+df_zad_6_sort_index = df_zad_6_sort.sort_index()
+print("Zadanie 6 - sort index \n", df_zad_6_sort_index)
+
+
+#wykres pudelkowy
+plt.boxplot(df_zad_6_sort_index)
+plt.show()
